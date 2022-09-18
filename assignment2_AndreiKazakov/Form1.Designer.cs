cc14953c7bd2cdc69dc3e79822d3811ecfaf24d0
Andrei Kazakov <nvgator@gmail.com>
2022-09-18 13:50:20 +0300

    Revenue Generated GroupBox design

--- assignment2_AndreiKazakov/Form1.Designer.cs
+++ assignment2_AndreiKazakov/Form1.Designer.cs
@@ -29,23 +29,34 @@
         private void InitializeComponent()
         {
             this.InputGroupBox = new System.Windows.Forms.GroupBox();
+            this.ClassCInputLabel = new System.Windows.Forms.Label();
+            this.ClassBInputLabel = new System.Windows.Forms.Label();
+            this.ClassAInputLabel = new System.Windows.Forms.Label();
+            this.ClassCInputTextBox = new System.Windows.Forms.TextBox();
+            this.ClassBInputTextBox = new System.Windows.Forms.TextBox();
+            this.ClassAInputTextBox = new System.Windows.Forms.TextBox();
             this.EnterNumberOfTicketsLabel = new System.Windows.Forms.Label();
             this.OutputGroupBox = new System.Windows.Forms.GroupBox();
-            this.ClassAInputTextbot = new System.Windows.Forms.TextBox();
-            this.ClassBInputTextBot = new System.Windows.Forms.TextBox();
-            this.ClassCInputTextBox = new System.Windows.Forms.TextBox();
-            this.ClassAInputLabel = new System.Windows.Forms.Label();
-            this.ClassBInputLabel = new System.Windows.Forms.Label();
+            this.TotalOutputLabel = new System.Windows.Forms.Label();
+            this.TotalOutputTextBox = new System.Windows.Forms.TextBox();
+            this.ClassCOutputLabel = new System.Windows.Forms.Label();
+            this.ClassBOutputLabel = new System.Windows.Forms.Label();
+            this.ClassAOutputLabel = new System.Windows.Forms.Label();
+            this.ClassCOutputTextBox = new System.Windows.Forms.TextBox();
+            this.ClassBOutputTextBox = new System.Windows.Forms.TextBox();
+            this.ClassAOutputTextBox = new System.Windows.Forms.TextBox();
             this.InputGroupBox.SuspendLayout();
+            this.OutputGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // InputGroupBox
             // 
+            this.InputGroupBox.Controls.Add(this.ClassCInputLabel);
             this.InputGroupBox.Controls.Add(this.ClassBInputLabel);
             this.InputGroupBox.Controls.Add(this.ClassAInputLabel);
             this.InputGroupBox.Controls.Add(this.ClassCInputTextBox);
-            this.InputGroupBox.Controls.Add(this.ClassBInputTextBot);
-            this.InputGroupBox.Controls.Add(this.ClassAInputTextbot);
+            this.InputGroupBox.Controls.Add(this.ClassBInputTextBox);
+            this.InputGroupBox.Controls.Add(this.ClassAInputTextBox);
             this.InputGroupBox.Controls.Add(this.EnterNumberOfTicketsLabel);
             this.InputGroupBox.Location = new System.Drawing.Point(13, 13);
             this.InputGroupBox.Name = "InputGroupBox";
@@ -53,6 +64,54 @@
             this.InputGroupBox.TabIndex = 0;
             this.InputGroupBox.TabStop = false;
             this.InputGroupBox.Text = "Tickets Sold";
+            // 
+            // ClassCInputLabel
+            // 
+            this.ClassCInputLabel.AutoSize = true;
+            this.ClassCInputLabel.Location = new System.Drawing.Point(20, 172);
+            this.ClassCInputLabel.Name = "ClassCInputLabel";
+            this.ClassCInputLabel.Size = new System.Drawing.Size(45, 13);
+            this.ClassCInputLabel.TabIndex = 6;
+            this.ClassCInputLabel.Text = "Class C:";
+            // 
+            // ClassBInputLabel
+            // 
+            this.ClassBInputLabel.AutoSize = true;
+            this.ClassBInputLabel.Location = new System.Drawing.Point(20, 133);
+            this.ClassBInputLabel.Name = "ClassBInputLabel";
+            this.ClassBInputLabel.Size = new System.Drawing.Size(45, 13);
+            this.ClassBInputLabel.TabIndex = 5;
+            this.ClassBInputLabel.Text = "Class B:";
+            // 
+            // ClassAInputLabel
+            // 
+            this.ClassAInputLabel.AutoSize = true;
+            this.ClassAInputLabel.Location = new System.Drawing.Point(20, 97);
+            this.ClassAInputLabel.Name = "ClassAInputLabel";
+            this.ClassAInputLabel.Size = new System.Drawing.Size(45, 13);
+            this.ClassAInputLabel.TabIndex = 4;
+            this.ClassAInputLabel.Text = "Class A:";
+            // 
+            // ClassCInputTextBox
+            // 
+            this.ClassCInputTextBox.Location = new System.Drawing.Point(73, 172);
+            this.ClassCInputTextBox.Name = "ClassCInputTextBox";
+            this.ClassCInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClassCInputTextBox.TabIndex = 3;
+            // 
+            // ClassBInputTextBox
+            // 
+            this.ClassBInputTextBox.Location = new System.Drawing.Point(73, 133);
+            this.ClassBInputTextBox.Name = "ClassBInputTextBox";
+            this.ClassBInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClassBInputTextBox.TabIndex = 2;
+            // 
+            // ClassAInputTextBox
+            // 
+            this.ClassAInputTextBox.Location = new System.Drawing.Point(73, 97);
+            this.ClassAInputTextBox.Name = "ClassAInputTextBox";
+            this.ClassAInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClassAInputTextBox.TabIndex = 1;
             // 
             // EnterNumberOfTicketsLabel
             // 
@@ -65,6 +124,14 @@
             // 
             // OutputGroupBox
             // 
+            this.OutputGroupBox.Controls.Add(this.TotalOutputLabel);
+            this.OutputGroupBox.Controls.Add(this.TotalOutputTextBox);
+            this.OutputGroupBox.Controls.Add(this.ClassCOutputLabel);
+            this.OutputGroupBox.Controls.Add(this.ClassBOutputLabel);
+            this.OutputGroupBox.Controls.Add(this.ClassAOutputLabel);
+            this.OutputGroupBox.Controls.Add(this.ClassCOutputTextBox);
+            this.OutputGroupBox.Controls.Add(this.ClassBOutputTextBox);
+            this.OutputGroupBox.Controls.Add(this.ClassAOutputTextBox);
             this.OutputGroupBox.Location = new System.Drawing.Point(403, 13);
             this.OutputGroupBox.Name = "OutputGroupBox";
             this.OutputGroupBox.Size = new System.Drawing.Size(384, 288);
@@ -72,44 +139,73 @@
             this.OutputGroupBox.TabStop = false;
             this.OutputGroupBox.Text = "Revenue Generated";
             // 
-            // ClassAInputTextbot
+            // TotalOutputLabel
             // 
-            this.ClassAInputTextbot.Location = new System.Drawing.Point(73, 97);
-            this.ClassAInputTextbot.Name = "ClassAInputTextbot";
-            this.ClassAInputTextbot.Size = new System.Drawing.Size(100, 20);
-            this.ClassAInputTextbot.TabIndex = 1;
+            this.TotalOutputLabel.AutoSize = true;
+            this.TotalOutputLabel.Location = new System.Drawing.Point(75, 198);
+            this.TotalOutputLabel.Name = "TotalOutputLabel";
+            this.TotalOutputLabel.Size = new System.Drawing.Size(34, 13);
+            this.TotalOutputLabel.TabIndex = 14;
+            this.TotalOutputLabel.Text = "Total:";
             // 
-            // ClassBInputTextBot
+            // TotalOutputTextBox
             // 
-            this.ClassBInputTextBot.Location = new System.Drawing.Point(72, 133);
-            this.ClassBInputTextBot.Name = "ClassBInputTextBot";
-            this.ClassBInputTextBot.Size = new System.Drawing.Size(100, 20);
-            this.ClassBInputTextBot.TabIndex = 2;
+            this.TotalOutputTextBox.Enabled = false;
+            this.TotalOutputTextBox.Location = new System.Drawing.Point(128, 198);
+            this.TotalOutputTextBox.Name = "TotalOutputTextBox";
+            this.TotalOutputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TotalOutputTextBox.TabIndex = 13;
             // 
-            // ClassCInputTextBox
+            // ClassCOutputLabel
             // 
-            this.ClassCInputTextBox.Location = new System.Drawing.Point(71, 172);
-            this.ClassCInputTextBox.Name = "ClassCInputTextBox";
-            this.ClassCInputTextBox.Size = new System.Drawing.Size(100, 20);
-            this.ClassCInputTextBox.TabIndex = 3;
+            this.ClassCOutputLabel.AutoSize = true;
+            this.ClassCOutputLabel.Location = new System.Drawing.Point(75, 165);
+            this.ClassCOutputLabel.Name = "ClassCOutputLabel";
+            this.ClassCOutputLabel.Size = new System.Drawing.Size(45, 13);
+            this.ClassCOutputLabel.TabIndex = 12;
+            this.ClassCOutputLabel.Text = "Class C:";
             // 
-            // ClassAInputLabel
+            // ClassBOutputLabel
             // 
-            this.ClassAInputLabel.AutoSize = true;
-            this.ClassAInputLabel.Location = new System.Drawing.Point(20, 97);
-            this.ClassAInputLabel.Name = "ClassAInputLabel";
-            this.ClassAInputLabel.Size = new System.Drawing.Size(45, 13);
-            this.ClassAInputLabel.TabIndex = 4;
-            this.ClassAInputLabel.Text = "Class A:";
+            this.ClassBOutputLabel.AutoSize = true;
+            this.ClassBOutputLabel.Location = new System.Drawing.Point(75, 133);
+            this.ClassBOutputLabel.Name = "ClassBOutputLabel";
+            this.ClassBOutputLabel.Size = new System.Drawing.Size(45, 13);
+            this.ClassBOutputLabel.TabIndex = 11;
+            this.ClassBOutputLabel.Text = "Class B:";
             // 
-            // ClassBInputLabel
+            // ClassAOutputLabel
             // 
-            this.ClassBInputLabel.AutoSize = true;
-            this.ClassBInputLabel.Location = new System.Drawing.Point(20, 133);
-            this.ClassBInputLabel.Name = "ClassBInputLabel";
-            this.ClassBInputLabel.Size = new System.Drawing.Size(35, 13);
-            this.ClassBInputLabel.TabIndex = 5;
-            this.ClassBInputLabel.Text = "label1";
+            this.ClassAOutputLabel.AutoSize = true;
+            this.ClassAOutputLabel.Location = new System.Drawing.Point(75, 97);
+            this.ClassAOutputLabel.Name = "ClassAOutputLabel";
+            this.ClassAOutputLabel.Size = new System.Drawing.Size(45, 13);
+            this.ClassAOutputLabel.TabIndex = 10;
+            this.ClassAOutputLabel.Text = "Class A:";
+            // 
+            // ClassCOutputTextBox
+            // 
+            this.ClassCOutputTextBox.Enabled = false;
+            this.ClassCOutputTextBox.Location = new System.Drawing.Point(128, 165);
+            this.ClassCOutputTextBox.Name = "ClassCOutputTextBox";
+            this.ClassCOutputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClassCOutputTextBox.TabIndex = 9;
+            // 
+            // ClassBOutputTextBox
+            // 
+            this.ClassBOutputTextBox.Enabled = false;
+            this.ClassBOutputTextBox.Location = new System.Drawing.Point(128, 133);
+            this.ClassBOutputTextBox.Name = "ClassBOutputTextBox";
+            this.ClassBOutputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClassBOutputTextBox.TabIndex = 8;
+            // 
+            // ClassAOutputTextBox
+            // 
+            this.ClassAOutputTextBox.Enabled = false;
+            this.ClassAOutputTextBox.Location = new System.Drawing.Point(128, 97);
+            this.ClassAOutputTextBox.Name = "ClassAOutputTextBox";
+            this.ClassAOutputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClassAOutputTextBox.TabIndex = 7;
             // 
             // Form1
             // 
@@ -122,6 +218,8 @@
             this.Text = "Stadium Seating";
             this.InputGroupBox.ResumeLayout(false);
             this.InputGroupBox.PerformLayout();
+            this.OutputGroupBox.ResumeLayout(false);
+            this.OutputGroupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -134,8 +232,17 @@
         private System.Windows.Forms.Label ClassBInputLabel;
         private System.Windows.Forms.Label ClassAInputLabel;
         private System.Windows.Forms.TextBox ClassCInputTextBox;
-        private System.Windows.Forms.TextBox ClassBInputTextBot;
-        private System.Windows.Forms.TextBox ClassAInputTextbot;
+        private System.Windows.Forms.TextBox ClassBInputTextBox;
+        private System.Windows.Forms.TextBox ClassAInputTextBox;
+        private System.Windows.Forms.Label ClassCInputLabel;
+        private System.Windows.Forms.Label ClassCOutputLabel;
+        private System.Windows.Forms.Label ClassBOutputLabel;
+        private System.Windows.Forms.Label ClassAOutputLabel;
+        private System.Windows.Forms.TextBox ClassCOutputTextBox;
+        private System.Windows.Forms.TextBox ClassBOutputTextBox;
+        private System.Windows.Forms.TextBox ClassAOutputTextBox;
+        private System.Windows.Forms.Label TotalOutputLabel;
+        private System.Windows.Forms.TextBox TotalOutputTextBox;
     }
 }
 
